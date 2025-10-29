aca44c86cf5024c4e3fc621b112e82de7c18f1ac
Moxtil <sang.j52016@gmail.com>
2025-10-29 22:53:49 +0300

    Loader

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,7 @@
 import { useUser } from "@clerk/nextjs";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { LoadingSkeleton } from "./components/LoadingSkeleton";
-import { LargeLoadingSkeleton } from "./components/LargeLoadingSkeleton";
-import { GPTLoaderSkeleton } from "./components/GPTLoaderSkeleton";
+import GPTLoaderSkeleton from "./components/GPTLoaderSkeleton";
 export default function Home() {
   const { user, isLoaded, isSignedIn } = useUser();
   const router = useRouter();
